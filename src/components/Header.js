2d5f3beae0492a010578a84ff8575002715a24a9
chojiu <yacine@wildcodeschool.fr>
2020-04-17 11:28:51 +0200

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Header extends React.Component {
+    render () {
+    return (
+    <>
+        <h1>Test</h1>
+        <h2>Test2</h2>
+
+    </>
+
+    )}
+}
+
+export default Header
