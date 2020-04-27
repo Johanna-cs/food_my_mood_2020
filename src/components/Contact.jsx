35669e0b4a7da2c3f70b48654dc0412d9a65e1cd
johanna-cs <johanna.cas@icloud.com>
2020-04-27 09:59:51 +0200

    bug correction on navbar

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import './headerFooter/custom.scss'
 
 const Contact = () => {
     return (
-<h4> Contactez nous ! </h4> 
-    )}
-
+    <div className='contact'>
+        <h4> Contactez nous ! </h4> 
+    </div>
+        )}
 export default Contact
