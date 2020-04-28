3e4861736ab249dfbba815431861b80449d575ca
jbb-dev <jbbouillat@yahoo.fr>
2020-04-28 16:03:12 +0200

    fix errors after global merging

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -6,18 +6,14 @@ import PreferenceList from './pageSelect/PreferencesList'
 import Contact from './Contact'
 import Header from './headerFooter/Header'
 import Footer from './headerFooter/Footer'
-<<<<<<< HEAD
 import RecipeDetails from './pageRecipe/RecipeDetails'
-=======
 import PreferencesList from './pageSelect/PreferencesList'
->>>>>>> ba20f81b81b7982dffe9311cf8d9f1f8b28983d3
 
 
 
 const Router = () => {
     return (
         <>
-<<<<<<< HEAD
         
         <Header />
         <Switch>
@@ -27,27 +23,9 @@ const Router = () => {
                     <Route exact path ='/results' component={pageResults} />
                     <Route exact path ='/contact' component={Contact} />
                     <Route path='/results/:id' component={RecipeDetails} />
-<<<<<<< HEAD
-=======
-
->>>>>>> aa568fff01c53bd6db13812145628aa3e0294d7f
             </div>
         </Switch>
         <Footer />
-=======
-            <Header />
-            <Switch>
-                <div className='routerLinks'>
-                    <Route exact path='/' component={MoodsList} />
-                    <Route exact path='/select' component={PreferencesList} />
-                    <Route exact path='/results' component={pageResults} />
-                    <Route exact path='/contact' component={Contact} />
-
-
-                </div>
-            </Switch>
-            <Footer />
->>>>>>> ba20f81b81b7982dffe9311cf8d9f1f8b28983d3
         </>
 
     )
