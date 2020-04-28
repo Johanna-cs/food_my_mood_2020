565dec952cb1d36cc883e6f814927eb657543f9f
johanna-cs <johanna.cas@icloud.com>
2020-04-28 14:56:13 +0200

    create the filter component but not used for now

---
+++ src/components/pageResults/Filter.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+
+
+
+class Filter extends Component {
+
+    render() {
+        return (
+            <>
+            <div>
+                <h3>Filters</h3>
+
+
+                <div className='filterTime'>
+                    <form className='timing'>
+                        <label id='timing' for="Timing">Timing</label>
+                            <select type="select" className="timing">
+                            <option>10min</option>
+                            <option>30min</option>
+                            <option>45min</option>
+                            <option>1h </option>
+                            <option>2h +</option>
+                            </select>
+                    </form>
+                </div>
+
+              </div>  
+            </>
+        )
+    }
+
+    
+}
+
+export default Filter
