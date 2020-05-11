4d456aeaee5d9554697c1dcd766ee123224c185e
lco23 <olivierbaveux@hotmail.com>
2020-05-11 14:54:58 +0200

    search bar

--- src/components/pageResults/Filter.js
+++ src/components/pageResults/Filter.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react'
-
+import { Link } from 'react-router-dom'
 
 
 class Filter extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            search : ""
+        }
+    }
 
     render() {
+
+ //       const filterQueries = recipes.filter(query =>
+ //           query.toLowerCase().includes (this.search.toLowerCase())
+ //       )
+
         return (
             <>
             <div>
                 <h3>Filters</h3>
 
+                <input type="text" placeholder= "Search" onChange={ e => this.setState (
+                    {
+                       search : e.target.value
+                    }
+                    )}/>
 
+                <Link to ={{pathname: '/results',
+                               ingredient: this.state.search }}>
+                    <button>Valider</button>
+                </Link> 
+
+                    
                 <div className='filterTime'>
                     <form className='timing'>
                         <label id='timing' for="Timing">Timing</label>
@@ -33,3 +55,17 @@ class Filter extends Component {
 }
 
 export default Filter
+
+//    {this.state.filterQueries.map(recip => recip.recipe).map(e=> (
+            //        <RecipeCard 
+            //        label={e.label} 
+            //        image={e.image} 
+            //        time={e.totalTime} 
+            //        calories={e.calories}
+            //        uri={e.uri}
+            //        recipes={this.state.recipes.map(recipe=>recipe.recipe)}
+            //        query={e.q} 
+            //        />
+            //    ))}
+
+              //.search(e.target.value) }/>
